508401247501b15b87af81123ad1054732f62d15
Kondratev Aleksandr <wisp.1991@gmail.com>
2023-07-26 13:43:51 +0300

    div

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+<<<<<<< HEAD
 
 public class Main {
     public static void main(String[] args) {
 
     }
+=======
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        String folderPath = "D:\\Java_2";
+        File file = new File(folderPath);
+
+        System.out.println(getFolderSize(file));
+
+    }
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
+    }
+>>>>>>> 31e00df (div)
 }
